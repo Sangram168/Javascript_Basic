3fe78174f0f570e4529bda7eaca7e4cf3697116b
Sangram <sangram1677@gmail.com>
2023-08-13 01:17:39 +0530

    variables

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hello world")
+
+// Variables (Primitive type)
+
+let a = 23;
+console.log(a);
+
+let b = true;
+console.log(b);
+
+let c = 'Rohit';
+console.log(c);
+
+let d = 12.3;
+console.log(d);
+
+let e = 23.45678;
+console.log(e);
+
+let undefine;
+
+let nl = null;
+console.log(nl);
