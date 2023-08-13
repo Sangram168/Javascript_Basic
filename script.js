2356a4dc525b768445dc75bc9b7da7f3b0dd5402
Sangram <sangram1677@gmail.com>
2023-08-13 19:48:21 +0530

    operators

--- script.js
+++ script.js
@@ -45,3 +45,90 @@ console.log(person['age']);
 
 let names = ['Aman', 'Rohit', 'Arjun'];
 console.log(names);
+
+// operator
+
+// Arithmetic(+,-,*,/,%)
+
+let aa = 6;
+let bb = 2;
+console.log(aa+bb);
+console.log(aa-bb);
+console.log(aa*bb);
+console.log(aa/bb);
+console.log(aa%bb);
+console.log(aa**bb);
+
+// Preincrement/Postincrement
+
+console.log(++aa);
+console.log(bb++);
+console.log(bb);
+
+// Preincrement/Postdecrement
+
+console.log(--aa);
+console.log(bb--);
+console.log(bb);
+
+// Assignment operator
+
+let cc = 2;
+c = c+2;
+console.log(c);
+
+// Comparison operator
+
+let dd = 23;
+let ee = 12;
+
+console.log(dd>ee);
+console.log(dd<ee);
+console.log(dd>=ee);
+console.log(dd<=ee);
+console.log(dd===ee); //(strict equality) - datatype and value both needs to be equal
+console.log(dd!==ee);
+
+//Ternary Operator - It is also known as condition operator
+
+let age = 17;
+let status = (age >= 18)? 'Yes vote' : 'No vote';
+console.log(status);
+
+// Logical Operator - It allows us to compare variables or values and mostly used to 
+// make decisions based on condition specified for the statements.
+
+// And(&&) - all condition must be true
+// oR - Atleast one condition must be true
+// Not(!) - true become false and false become true
+let x = 23;
+let y = 12;
+console.log(x>11 && y<13);
+console.log(x == 23 || y == 6);
+console.log(!x>13);
+
+// With Non-Booleans - concept of false and truth
+// falsy value - undefined, null, 0, false, NaN
+// truthy value - anything not falsy
+
+console.log(true || null);
+console.log(true || 'Aman');
+
+// Bitwise Operator - It work on bit
+// Bitwise And(&)
+// Bitwise Or(|)
+
+let op = 2;
+let op2 = 3;
+console.log(op&op2);
+console.log(op|op2);
+
+
+
+
+
+
+
+
+
+
